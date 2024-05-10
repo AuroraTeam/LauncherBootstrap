4438eb3a38fc2ab42eea03d916c8fd07d333f8f3
Qwertbun <81864346+Qwertbun@users.noreply.github.com>
2024-05-11 02:57:24 +0300

    Update launcher_bootstrap.dart

--- bin/launcher_bootstrap.dart
+++ bin/launcher_bootstrap.dart
@@ -3,3 +3,20 @@ import 'package:launcher_bootstrap/app.dart';
 void main(List<String> arguments) {
   App.run(arguments);
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          // use AppIcon to show your application icon
+          child: AppIconImage(),
+        ),
+      ),
+    );
+  }
+}
